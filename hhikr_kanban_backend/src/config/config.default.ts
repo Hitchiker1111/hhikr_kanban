4b33c9a5ab7beab2da0423720de1ed86e84826ec
Hhikr <1879735766@qq.com>
2024-08-07 20:29:42 +0800

    初始化前后端框架

---
+++ hhikr_kanban_backend/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1723020463209_6548',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
